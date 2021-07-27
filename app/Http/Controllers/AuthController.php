50db94853c6241fafcc9403d973f3e027fc2d589
Iloba <scarlettsney@gmail.com>
2021-07-27 09:18:33 +0100

    api-completed

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,9 +2,71 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
-    //
+    //register
+    public function register(Request $request){
+       $fields = $request->validate([
+            'name' => 'required',
+            'email' => 'required|string|unique:users,email',
+            'password' => 'required|string|confirmed' 
+       ]);
+
+       $user = User::create([
+            'name' => $fields['name'],
+            'email' => $fields['email'],
+            'password' => Hash::make($fields['name']),
+       ]);
+
+       $token = $user->createToken('myapptoken')->plainTextToken;
+
+       $response = [
+            'user' => $user,
+            'token' => $token
+       ];
+
+       return response($response, 201);
+    }
+
+
+    
+    public function login(Request $request){
+        $fields = $request->validate([
+             'email' => 'required|string',
+             'password' => 'required|string' 
+        ]);
+ 
+        //check email
+        $user = User::where('email', $fields['email'])->first();
+
+        //check password
+        if(!$user || !Hash::check($fields['password'], $user->password)){
+            return response([
+                'message' => 'Bad Credentials'
+            ]);
+        }
+ 
+        $token = $user->createToken('myapptoken')->plainTextToken;
+ 
+        $response = [
+             'user' => $user,
+             'token' => $token
+        ];
+ 
+        return response($response, 201);
+     }
+
+
+    public function logout(Request $request){
+        auth()->user()->token()->delete();
+
+        return [
+            'message' => 'Logged Out'
+        ];
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ProductController;
 
 
@@ -26,11 +27,15 @@ Route::group(['middleware' => ['auth:sanctum']], function () {
     Route::post('/products', 'ProductController@store');
     Route::put('/products/{id}', 'ProductController@update');
     Route::delete('/products/{id}', 'ProductController@destroy');
+    Route::post('/logout', 'AuthController@logout');
 });
 
 // Route::resource('products', ProductController::class);
 Route::get('/products', 'ProductController@index');
 Route::get('/products/{id}', 'ProductController@show');
+Route::post('/register', 'AuthController@register');
+Route::post('/login', 'AuthController@login');
+
 
 //after uncommenting in my route service provider class
 Route::get('/products/search/{name}', 'ProductController@search');
